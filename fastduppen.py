af9738072608e256b01f8ca4569f378ea3a83f12
Lior Rozin <93065123+Zayatsoff@users.noreply.github.com>
2023-01-26 14:45:44 -0500

    Update fastduppen.py

--- fastduppen.py
+++ fastduppen.py
@@ -1,2 +1,6 @@
 import fastdup
 fastdup.run("/Users/liorrozin/Desktop/2.0")
+fastdup.create_duplicates_gallery('similarity.csv',save_path='.', num_images=20, max_width=400)
+from IPython.display import HTML
+HTML('similarity.html')
+print("Done")
